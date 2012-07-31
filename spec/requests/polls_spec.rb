6e7257dd1aaa1d694f79b5ab22f1651e790b7fe0
Apprentice <apprentice@devbootcamp.com>
2012-07-31 16:49:22 -0700

    tests for poll class

--- spec/requests/polls_spec.rb
+++ spec/requests/polls_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe "Polls" do
-  describe "GET /polls" do
-    it "works! (now write some real specs)" do
+  describe "GET /polls/new" do
+    it "routes correctly" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get polls_path
+      get new_poll_path
       response.status.should be(200)
     end
   end
+
+  describe "#create_slug"do
+    before(:each) do
+      @poll = Poll.new
+      @poll.create_slug
+    end
+
+    it "creates a slug" do
+      @poll.create_slug.should be
+    end
+
+    it "creates a slug that consists of 7 characters" do
+      @poll.slug.length.should == 7
+    end
+
+    it "creates a slug that only contains letters" do
+      @poll.slug.should match /^[a-zA-Z]+$/
+    end
+
+
+  end
+
 end
